f41a658a7e9b1f0e7e66e36d5ac70267748982bd
pandanati <72080264+pandanati@users.noreply.github.com>
2021-09-06 18:52:43 +0200

    Added Class ClassRoom with instance fields, list and properties

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        #region Instance Fields
+        private string _className;
+        private DateTime _semesterStart;
+        private List<Student> _classList;
+        #endregion
+
+        #region Constructor
+        public ClassRoom()
+        {
+            
+        }
+        #endregion
+
+        #region Properties
+        public string ClassName
+        {
+            get { return _className; }
+            set { _className = value; }
+        }
+
+        public DateTime SemesterStart
+        {
+            get { return _semesterStart; }
+            set { _semesterStart = value; }
+        }
+
+        public List<Student> ClassList
+        {
+            get { return _classList; }
+            set { _classList = value; }
+        }
+        #endregion
+
+
+    }
+}
